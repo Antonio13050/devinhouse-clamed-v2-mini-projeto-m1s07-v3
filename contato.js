4c8a213314f44026848b83c58a4c31f105c356c9
Antonio Carlos Virissimo <antonio13050@gmail.com>
2023-07-03 11:53:59 -0700

    Ex6 - [pagina-contato] Adicionar Evento de envio de Formulário de Contato

--- contato.js
+++ contato.js
@@ -20,3 +20,33 @@ listaAssuntos.forEach((assunto) => {
 
     selectElement.appendChild(optionsElement);
 });
+
+const formulario = document.querySelector("#form-contato");
+
+formulario.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let nome = document.querySelector("#nome").value;
+    let telefone = document.querySelector("#telefone").value;
+    let email = document.querySelector("#email").value;
+    let assunto = document.querySelector("#assunto").value;
+    let mensagem = document.querySelector("#mensagem").value;
+    let orcamento = document.querySelector("#orcamento").checked
+        ? "Sim"
+        : "Não";
+
+    let body = `Nome: ${nome}\nTelefone: ${telefone}\nEmail: ${email}\nAssunto: ${assunto}\nOrçamento: ${orcamento} \nMensagem: ${mensagem}`;
+
+    Email.send({
+        SecureToken: "7d4abb57-fc7a-482f-af11-505ba9b708e0",
+        // Host: "smtp.elasticemail.com",
+        // Username: "antonio13050@gmail.com",
+        // Password: "98C8CFD66FBE288CDE79D83F60C3A3508F51",
+        To: "antonio13050@gmail.com",
+        From: "antonio13050@gmail.com",
+        Subject: "E-mail do formulario do site FarmaDev",
+        Body: body,
+    }).then(() => alert("E-mail enviado com sucesso"));
+
+    formulario.reset();
+});
