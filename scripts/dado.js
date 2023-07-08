1d676b2cf18acd03863fd3d1e4e8cdaba7c4feab
Antonio Carlos Virissimo <antonio13050@gmail.com>
2023-07-08 16:43:08 -0700

    [M1S08] Ex 1 - [Dados] Criando um JSON com os dados

---
+++ scripts/dado.js
@@ -0,0 +1,19 @@
+const usuarios = `
+  [
+    {
+        "email": "teste@email.com",
+        "nome": "Admin",
+        "senha" : 1234
+    },
+    {
+        "nome": "Antonio",
+        "email": "antonio@email.com",
+        "senha" : 1234
+    },
+    {
+        "nome": "Felipe",
+        "email": "felipe@email.com",
+        "senha" : 1234
+    }
+  ]
+`;
